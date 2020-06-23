dae7474f3e487bdf8371f8a9930e69774d0a1e45
Tauassar <61244479+Tauassar@users.noreply.github.com>
2020-06-24 00:04:57 +0600

    ex4 finished

--- ex4/script.js
+++ ex4/script.js
@@ -1,27 +1,35 @@
 //Choose a random color
 /* eslint-env es6 */
 /* eslint-disable no-console */
-const button = document.querySelector('#button')
-const input = document.querySelector('#fname')
-const error = document.querySelector('.error')
+const buttonPrev = document.querySelector('#buttonPrev')
+const buttonNext = document.querySelector('#buttonNext')
+const input = document.querySelector('#slide')
 
-input.addEventListener('keydown', logKey);
+var counter = 0
+images=['assets/img/1.jpg','assets/img/2.jpg','assets/img/3.jpg','assets/img/4.jpg','assets/img/5.jpg','assets/img/6.jpg',]
 
-function logKey(e) {
-  if(e.keyCode==13){
-      submit()
-  }
-}
-button.addEventListener('click', submit)
-
-
-function submit(){
-    if(input.value==''){
-        error.classList.add('show')
-        setTimeout(function(){
-        error.classList.remove('show')
-        }, 2000)
-    }else{
-        document.getElementById('message').innerHTML = input.value
+buttonNext.addEventListener('click', next)
+function next(){
+    if(counter<images.length){
+ document.getElementById('slide').setAttribute('src',images[counter])
+    counter++
+    }
+    else{
+        counter=0;
+        next()
     }
 }
+
+buttonPrev.addEventListener('click', prev)
+function prev(){
+    if(counter>0){
+    counter--
+ document.getElementById('slide').setAttribute('src',images[counter])
+    }
+    else{
+        counter=images.length;
+        prev()
+    }
+}
+
+
