1ae8cb9eb6a0d0d3f6c7b0ffa26f9ee784733697
Tauassar <Tauassar.tatiyev@nu.edu.kz>
2020-06-26 20:40:26 +0600

    ex5 filter js  complete

---
+++ ex5/button_logic.js
@@ -0,0 +1,52 @@
+(function() {
+  const buttons = document.querySelectorAll('.btn_filter');
+
+  buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+        clearDOM();
+      if (button.classList.contains('dark')){
+          console.log('dark choosen')
+        var i;
+        for(i=0; i<beers.length; i++){
+            
+            if(beers[i].dark){console.log('dark chosen');createCard(i);}
+        }
+      }
+      if (button.classList.contains('bright')){
+        var i;
+        for(i=0; i<beers.length; i++){
+            if(!beers[i].dark){console.log('bright chosen');createCard(i);}
+        }
+      }
+      if (button.classList.contains('bottle')){
+        var i;
+        for(i=0; i<beers.length; i++){
+            if(beers[i].bottle){console.log('bottle chosen');createCard(i);}
+        }
+      }
+      if (button.classList.contains('jar')){
+        var i;
+        for(i=0; i<beers.length; i++){
+            if(!beers[i].bottle){console.log('jar chosen');createCard(i);}
+        }
+      }
+      if (button.classList.contains('fagot')){
+        var i;
+        for(i=0; i<beers.length; i++){
+            if(beers[i].fagot){console.log('fagot chosen');createCard(i);}
+        }
+      }
+        createCard(i);
+    })
+  })
+})();
+
+
+function clearDOM(){
+    const cards = document.getElementById('cards');
+
+  while (cards.firstChild) {
+    cards.removeChild(cards.lastChild);
+  }
+    console.log('clear complete')
+}
